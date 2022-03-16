15d382203f1855442ba0f020a97523d5cf3ac074
Dmitry Vasylenko <80789887+RossTDE@users.noreply.github.com>
2022-03-16 22:10:31 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using Crestic;
+
+//Это пока оооооочень приблизительный набросок
+
+Coord Computer(Field pole)
+{
+    byte x, y;
+    Random rand = new Random();
+
+    do
+    {
+        x = (byte)rand.Next(0, 3);
+        y = (byte)rand.Next(0, 3);
+    } while (pole.field[x, y] != 0);
+
+    return new Coord(x, y);
+}
+
+Field pole = new Field();
+Coord Pcoord = new Coord(0,0);
+Coord Ccoord;
+
+do
+{
+
+    Console.Write("Enter first coord: ");
+    Pcoord.x = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Enter second coord: ");
+    Pcoord.y = Convert.ToInt32(Console.ReadLine());
+    pole.field[Pcoord.x, Pcoord.y] = 1;
+
+    Ccoord = Computer(pole);
+    pole.field[Ccoord.x, Ccoord.y] = 2;
+    pole.Print();
+
+} while (true);
---
+++ field.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crestic
+{
+    class Coord
+    {
+        public int x { get; set; } = 0;
+        public int y { get; set; } = 0;
+
+        public Coord(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    class Field
+    {
+       public byte[,] field = new byte[3,3]; //оказывается оно изначально зануляется
+
+        public void Print() {
+          for(int i = 0; i < 3; i++)
+          {
+                for (int j = 0; j < 3; j++)
+                {
+                    switch (field[i, j])
+                    {
+                        case 0: Console.Write("# ");
+                            break;
+                        case 1: Console.Write("X ");
+                            break;
+                        case 2: Console.Write("O ");
+                            break;
+                    }
+                    
+                }
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
